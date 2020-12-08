7c04dfcf64357214c974cf1a69cd22afd843a8f0
AwaleSajil <46675949+AwaleSajil@users.noreply.github.com>
2020-12-08 17:18:38 +0545

    sent also data_loader to baseTrainer

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -32,7 +32,7 @@ class Trainer(BaseTrainer):
 	"""
 	def __init__(self, model, loss, metrics, optimizer, resume, config,
 				 data_loader, valid_data_loader=None, lr_scheduler=None, train_logger=None):
-		super(Trainer, self).__init__(model, loss, metrics, optimizer, resume, config, train_logger)
+		super(Trainer, self).__init__(model, loss, metrics, optimizer, resume, config, train_logger, data_loader)
 		self.config = config
 		self.data_loader = data_loader
 		self.valid_data_loader = valid_data_loader
